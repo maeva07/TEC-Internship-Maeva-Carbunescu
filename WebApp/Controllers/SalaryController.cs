f1dcba2af13bc98ec5a3ebf9c997a66e8881eb64
Maeva Carbunescu <carbunescu_maeva@yahoo.ro>
2024-06-07 21:06:54 +0300

    Add project files.

---
+++ WebApp/Controllers/SalaryController.cs
@@ -0,0 +1,40 @@
+﻿using WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    public class SalaryController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<Salary> list = new List<Salary>();
+            HttpClient client = new HttpClient();
+            HttpResponseMessage message = await client.GetAsync("http://localhost:5229/api/Salaries");
+            if (message.IsSuccessStatusCode)
+            {
+                var jstring = await message.Content.ReadAsStringAsync();
+                list = JsonConvert.DeserializeObject<List<Salary>>(jstring);
+                return View(list);
+            }
+            else
+                return View(list);
+        }
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage message = await client.DeleteAsync("http://localhost:5229/api/Salaries/" + Id);
+            if (message.IsSuccessStatusCode)
+                return RedirectToAction("Index");
+            else
+                return View();
+
+        }
+    }
+}
